24a6b6abc738ceba28e3a829731166d284ad162e
Samy Shihata <samy.s.shihata@gmail.com>
2014-10-06 20:47:00 +0200

    Forming a solution EDIT main.lisp * added function form-solution that retraces steps of solutin from goal-node

--- main.lisp
+++ main.lisp
@@ -21,15 +21,24 @@
 
    (path-cost :accessor search-cost     ; a function to calculate the cost of reaching
               :initarg :costfun)        ; a node. Not sure how to abstract this, perhaps
-   ; we are taking it too literally ?
+                                        ; we are taking it too literally ?
    ))
 
 ; a general search function.
+; Input  - prp      : general search problem of class search-p
+;          qing-fun : a queueing function of children nodes
+;                     the determines the search strategy
+; Output - a list of operators to applied on the initial state
+;          to reach the goal. returns nil if no solution is found
+
 (defmethod general-search ((prp search-p) qing-fun)
   (let ((root (make-node :state (initial-state prp)
                          :depth 0
                          :cost  0)))
-    (search-helper prp (list root) qing-fun)))
+    (let ((goal-node (search-helper (prp (list root) qing-fun))))
+       (if (not goal-node)
+         (format t "No solution was found. ~% Try relaxing your problem constraints.")
+         (form-solution goal-node nil)))))
 
 ;; =========================================
 
@@ -37,13 +46,16 @@
 ;; =========================================
 
 (defun search-helper (prp queue qing-fun)
-  (let ((head (car queue)))
-    (if (funcall (search-goalp prp) head)
-      head 
-      (search-helper prp
-                     (funcall qing-fun (cdr queue) 
-                                       (expand head (search-ops prp) (search-cost prp)))
-                     qing-fun))))
+  (if (null queue)
+    nil
+    (let ((head (car queue)))
+      (if (funcall (search-goalp prp) head)
+        head 
+        (search-helper prp
+                       (funcall qing-fun 
+                                (cdr queue) 
+                                (expand head (search-ops prp) (search-cost prp)))
+                       qing-fun)))))
 
 (defun expand (head operators costfun)
   (mapcar #'(lambda (op) (let ((new-cost 0)
@@ -55,3 +67,8 @@
                                       :depth    (+ (node-depth head) 1)
                                       :cost     (funcall costfun (node-cost head) new-cost))))
           operators))
+
+(defun form-solution (node op-list)
+  (if (null (node-parent n))
+    op-list
+    (form-solution (node-parent n) (cons (node-operator n) op-list))))
